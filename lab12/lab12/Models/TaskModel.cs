b5fd0e1c63c36346e027f28642a835dfc299a08c
VSinche <115099082+Vitocho22@users.noreply.github.com>
2023-12-03 21:51:23 -0500

    Agregar archivos de proyecto.

---
+++ lab12/lab12/Models/TaskModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab12.Models
+{
+    public class TaskModel
+    {
+        public string Title { get; set; }
+        public bool Status { get; set; }
+        public string Description { get; set; }
+
+        public TaskModel(string title, bool status, string description)
+        {
+            Title = title;
+            Status = status;
+            Description = description;
+        }
+    }
+}
+
